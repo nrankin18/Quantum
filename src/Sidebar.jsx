b7dcf20f22a3616b387c25fec0a24e003e26dfde
Nathaniel Rankin <18nrankin@gmail.com>
2021-04-21 19:32:49 -0400

    CNOT styles

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,6 +6,16 @@ class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
+        <h1>Instructions:</h1>
+        <hr />
+        Drag and drop gates on qubit register lines to form a circuit.
+        <br />
+        <br />
+        To connect CNOT trigger lines, click on one of the connection points
+        that appear after adding a CNOT gate.
+        <br />
+        <br />
+        To delete a gate, double click on it.
         <h1>Debug:</h1>
         <hr />
         <label>
