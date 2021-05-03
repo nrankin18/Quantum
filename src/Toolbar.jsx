be175a647640204bc62d5a3aedbe0e995386c075
Nathaniel Rankin <18nrankin@gmail.com>
2021-05-03 15:04:26 -0400

    Version

--- src/Toolbar.jsx
+++ src/Toolbar.jsx
@@ -8,6 +8,16 @@ class Toolbar extends Component {
     return (
       <div style={{ display: "fixed", height: "50px" }}>
         <div className="toolbar">
+          <div
+            style={{
+              position: "absolute",
+              left: "25px",
+              top: "0",
+              fontSize: "0.8em",
+            }}
+          >
+            Version 1.4.6
+          </div>
           Spring 2021 IQC Quantum Computer Simulator
           <div style={{ position: "absolute", right: "40px", top: "0" }}>
             <Clock ticking format={"HH:mm:ss"} timezone={"Etc/UTC"} />
